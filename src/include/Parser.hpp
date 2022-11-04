43c52c0aa19d0457881ec5af16d957003e9cb916
cjrevelas <cjrevelas@gmail.com>
2022-11-04 20:23:52 +0200

    feat: declaration of Parser class

--- src/include/Parser.hpp
+++ src/include/Parser.hpp
@@ -1,14 +1,42 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
-namespace RusselNS {
-  class Parser {
-    private:
+#include <string>
+#include <vector>
+#include <deque>
+#include <memory>
 
-    public:
-     Parser() {}
-     ~Parser() {}
-  };
+#include "Russel.hpp"
+
+namespace RusselNS {
+
+class Parser {
+ protected:
+  std::string flag_;
+  std::string coeffs_;
+  int flagLength_;
+  int coeffsMinLength_;
+
+ public:
+  Parser(const std::string &flag, const int &coeffsMinLength);
+  virtual ~Parser() {}
+
+  std::string GetFlag() const { return flag_;}
+
+  bool GetCoeffs(const std::string &stringCoeffs) const;
+
+  virtual void ProcessCoeffs(const std::deque<std::string> &coeffs);
+
+  static int lineOfInput_;
+  static bool verbose_;
+  static std::unique_ptr<Russel> russel_;
+};
+
+void ParseInput(const std::string &filename, std::unique_ptr<Russel> &russel); // we pass a smart pointer by reference here
+
+void CheckDuplicateFlags(const std::vector<std::unique_ptr<Parser>> &listOfFlags); // we pass a vector of smart pointers by reference here
+
+std::string CheckForExpressions(const std::string &inputString);
 }
 
 #endif
