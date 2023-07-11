573e3d701313bf93e880317d41b564e4aecfbf18
cjrevelas <cjrevelas@gmail.com>
2023-07-11 13:29:09 +0300

    refactor: add constants and unit transformations

---
+++ src/include/Constants.hpp
@@ -0,0 +1,26 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+namespace RusselNS {
+
+  constexpr double boltz_const_Joule_molK = 8.3144598e0;
+  constexpr double boltz_const_Joule_K    = 1.38064852e-23;
+  constexpr double n_avog                 = 6.02214085700e23;
+  constexpr double pi                     = 3.14159265359e0;
+  constexpr double atm_to_pa              = 1.01325e5;
+  constexpr double tol                    = 1.0e-12;
+  constexpr double Ang_to_m               = 1.0e-10;
+  constexpr double Ang2_to_m2             = 1.0e-20;
+  constexpr double Ang3_to_m3             = 1.0e-30;
+  constexpr double m_to_Ang               = 1.0e10;
+  constexpr double m2_to_Ang2             = 1.0e20;
+  constexpr double m3_to_Ang3             = 1.0e30;
+  constexpr double m3_to_cm3              = 1.0e6;
+  constexpr double gr_cm3_to_kg_m3        = 1.0e3;
+  constexpr double kg_m3_to_gr_m3         = 1.0e3;
+  constexpr double N_to_mN                = 1.0e3;
+
+}
+
+
+#endif // CONSTANTS_HPP
